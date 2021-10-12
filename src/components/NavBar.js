9f058f3b0f4a6f0c7b94fc650b09b2848ba9760c
rodriguezyanet <yanet-rodriguez@outlook.com.ar>
2021-10-12 11:40:03 -0300

    header design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import logo from "../logo.png";
+//React fontawesome imports
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faBars} from "@fortawesome/free-solid-svg-icons";
+
 
 const NavBar = () => {
     return (
@@ -12,7 +16,8 @@ const NavBar = () => {
                         alt="Yanet Logo..." />
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon = {faBars} 
+                    style = {{ color: "#fff"}}/>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
